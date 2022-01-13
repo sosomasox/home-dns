0bd7760e87232c63de65171f651647b562585d97
Taichi Soma <sosomasox@gmail.com>
2022-01-13 19:55:44 +0900

    Fix deploy.sh

--- ns1.home.arpa/deploy.sh
+++ ns1.home.arpa/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
-sudo mv home.arpa.zone home.arpa.rev /var/cache/bind/
+sudo cp home.arpa.zone /var/cache/bind/home.arpa.zone
+sudo cp home.arpa.rev  /var/cache/bind/home.arpa.rev
 sudo systemctl restart bind9
 
 exit 0
