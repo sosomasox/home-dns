7324cf292fe088e3a8673795eb9a47fda20fad9e
Taichi Soma <sosomasox@gmail.com>
2022-01-15 10:55:34 +0900

    Update dns.k8s.home.arpa/deploy.sh

--- dns.k8s.home.arpa/deploy.sh
+++ dns.k8s.home.arpa/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+sudo cp named.conf.local   /etc/bind/named.conf.local
+sudo cp named.conf.options /etc/bind/named.conf.options
 sudo cp k8s.home.arpa.zone /var/cache/bind/k8s.home.arpa.zone
 sudo cp k8s.home.arpa.rev  /var/cache/bind/k8s.home.arpa.rev
 sudo systemctl restart bind9
