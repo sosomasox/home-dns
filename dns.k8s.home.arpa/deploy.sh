34c80b9f4e0e92ccc32ec47f1f54ac76e5695e7c
Taichi Soma <sosomasox@gmail.com>
2022-01-16 22:25:44 +0900

    Clean

--- dns.k8s.home.arpa/deploy.sh
+++ dns.k8s.home.arpa/deploy.sh
@@ -2,7 +2,7 @@
 
 diff named.conf.local /etc/bind/named.conf.local
 if [ "$?" != "0" ];then
-    sudo cp named.conf.local   /etc/bind/named.conf.local
+    sudo cp named.conf.local /etc/bind/named.conf.local
 fi
 
 diff named.conf.options /etc/bind/named.conf.options
@@ -15,9 +15,9 @@ if [ "$?" != "0" ];then
     sudo cp k8s.home.arpa.zone /var/cache/bind/k8s.home.arpa.zone
 fi
 
-diff k8s.home.arpa.rev  /var/cache/bind/k8s.home.arpa.rev
+diff k8s.home.arpa.rev /var/cache/bind/k8s.home.arpa.rev
 if [ "$?" != "0" ];then
-    sudo cp k8s.home.arpa.rev  /var/cache/bind/k8s.home.arpa.rev
+    sudo cp k8s.home.arpa.rev /var/cache/bind/k8s.home.arpa.rev
 fi
 
 sudo systemctl restart bind9
