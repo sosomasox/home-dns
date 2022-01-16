8a01078cafea807ce8af6a3e68a7f0268f3e4fa7
Taichi Soma <sosomasox@gmail.com>
2022-01-16 22:26:01 +0900

    Update ns-cache.home.arpa/deploy.sh

--- ns-cache.home.arpa/deploy.sh
+++ ns-cache.home.arpa/deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -x
 
-diff named.conf.local      /etc/bind/named.conf.local
-diff named.conf.options    /etc/bind/named.conf.options
-sudo cp named.conf.local   /etc/bind/named.conf.local
-sudo cp named.conf.options /etc/bind/named.conf.options
+diff named.conf.local /etc/bind/named.conf.local
+if [ "$?" != "0" ]; then
+    sudo cp named.conf.local /etc/bind/named.conf.local
+fi
+
+diff named.conf.options /etc/bind/named.conf.options
+if [ "$?" != "0" ]; then
+    sudo cp named.conf.options /etc/bind/named.conf.options
+fi
+
 sudo systemctl restart bind9
 
 exit 0
