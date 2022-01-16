70ccb695bcd54187fe3560ae3197209ff6322317
Taichi Soma <sosomasox@gmail.com>
2022-01-16 09:04:38 +0900

    Update to do diff

--- ns-cache.home.arpa/deploy.sh
+++ ns-cache.home.arpa/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+diff named.conf.local      /etc/bind/named.conf.local
+diff named.conf.options    /etc/bind/named.conf.options
 sudo cp named.conf.local   /etc/bind/named.conf.local
 sudo cp named.conf.options /etc/bind/named.conf.options
 sudo systemctl restart bind9
