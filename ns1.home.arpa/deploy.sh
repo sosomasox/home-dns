ee52be4751da3564ea76043d0334f0784efa294d
Taichi Soma <sosomasox@gmail.com>
2022-01-16 22:13:43 +0900

    Update ns1.home.arpa/deploy.sh

--- ns1.home.arpa/deploy.sh
+++ ns1.home.arpa/deploy.sh
@@ -1,9 +1,25 @@
 #!/bin/bash -x
 
-sudo cp named.conf.local   /etc/bind/named.conf.local
-sudo cp named.conf.options /etc/bind/named.conf.options
-sudo cp home.arpa.zone     /var/cache/bind/home.arpa.zone
-sudo cp home.arpa.rev      /var/cache/bind/home.arpa.rev
+diff named.conf.local /etc/bind/named.conf.local
+if [ "$?" != "0" ];then
+    sudo cp named.conf.local /etc/bind/named.conf.local
+fi
+
+diff named.conf.options /etc/bind/named.conf.options
+if [ "$?" != "0" ];then
+    sudo cp named.conf.options /etc/bind/named.conf.options
+fi
+
+diff home.arpa.zone /var/cache/bind/home.arpa.zone
+if [ "$?" != "0" ];then
+    sudo cp home.arpa.zone /var/cache/bind/home.arpa.zone
+fi
+
+diff home.arpa.rev /var/cache/bind/home.arpa.rev
+if [ "$?" != "0" ];then
+    sudo cp home.arpa.rev /var/cache/bind/home.arpa.rev
+fi
+
 sudo systemctl restart bind9
 
 exit 0
