9cdba22e01f128ab2087dc596b0f98e7bf3f0980
Taichi Soma <sosomasox@gmail.com>
2022-01-16 22:24:16 +0900

    Update dns.k8s.home.arpa/deploy.sh

--- dns.k8s.home.arpa/deploy.sh
+++ dns.k8s.home.arpa/deploy.sh
@@ -1,9 +1,25 @@
 #!/bin/bash -x
 
-sudo cp named.conf.local   /etc/bind/named.conf.local
-sudo cp named.conf.options /etc/bind/named.conf.options
-sudo cp k8s.home.arpa.zone /var/cache/bind/k8s.home.arpa.zone
-sudo cp k8s.home.arpa.rev  /var/cache/bind/k8s.home.arpa.rev
+diff named.conf.local /etc/bind/named.conf.local
+if [ "$?" != "0" ];then
+    sudo cp named.conf.local   /etc/bind/named.conf.local
+fi
+
+diff named.conf.options /etc/bind/named.conf.options
+if [ "$?" != "0" ];then
+    sudo cp named.conf.options /etc/bind/named.conf.options
+fi
+
+diff k8s.home.arpa.zone /var/cache/bind/k8s.home.arpa.zone
+if [ "$?" != "0" ];then
+    sudo cp k8s.home.arpa.zone /var/cache/bind/k8s.home.arpa.zone
+fi
+
+diff k8s.home.arpa.rev  /var/cache/bind/k8s.home.arpa.rev
+if [ "$?" != "0" ];then
+    sudo cp k8s.home.arpa.rev  /var/cache/bind/k8s.home.arpa.rev
+fi
+
 sudo systemctl restart bind9
 
 exit 0
